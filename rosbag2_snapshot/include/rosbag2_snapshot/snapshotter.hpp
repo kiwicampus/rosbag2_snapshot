3983e592ccd06b3f6156b4a759fd90405803ccc2
pepisg <pedro.gonzalez@eia.edu.co>
2023-07-27 21:29:43 +0000

    update cvbridge header file

--- rosbag2_snapshot/include/rosbag2_snapshot/snapshotter.hpp
+++ rosbag2_snapshot/include/rosbag2_snapshot/snapshotter.hpp
@@ -38,7 +38,7 @@
 #include <rosbag2_compression/sequential_compression_writer.hpp>
 #include <sensor_msgs/msg/image.hpp>
 #include <sensor_msgs/msg/compressed_image.hpp>
-#include <cv_bridge/cv_bridge.h>
+#include <cv_bridge/cv_bridge.hpp>
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <chrono>

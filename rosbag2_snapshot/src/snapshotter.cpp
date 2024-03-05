ff7e5f64083dbc03ece229def09a4b5794ce6e0a
salogranada <salogranda494@gmail.com>
2024-03-05 11:07:44 -0500

    docs

--- rosbag2_snapshot/src/snapshotter.cpp
+++ rosbag2_snapshot/src/snapshotter.cpp
@@ -249,6 +249,7 @@ MessageQueue::range_t MessageQueue::rangeFromTimes(Time const & start, Time cons
   // check that msg_time is within the range
   if (msg_time < start || msg_time > stop) {
     RCLCPP_ERROR(logger_, "msg_time is not within the range of start and stop times");
+    // We dont return as if we dont shorten the range the bag will be the size of the whole queue
   }
   
   if(options_.duration_limit_ != options_.NO_DURATION_LIMIT)
